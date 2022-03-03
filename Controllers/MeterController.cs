e4989a19b5195328f1b8e0293a1d2607c62460ea
HamidovOrkhn <orxan.hamidov.orxan.hamidov@mail.ru>
2022-03-03 17:22:31 +0400

    filter

--- Controllers/MeterController.cs
+++ Controllers/MeterController.cs
@@ -21,21 +21,22 @@ namespace Aim.Core.Services.Controllers
             _db = db;
         }
         [HttpGet("[action]")]
-        public IActionResult Get(int page = 0)
+        public IActionResult Get(int nodeId, string subid, int page = 0)
         {
             ResponseMessage<object> responseMessage = new();
             List<Meter> data = _db.Meters.ToList();
-            float pagecount = data.Count;
+            List<NodeMeter> nodeMeters = ExConverter.Filterize(_db.NodeMeters.ToList(), nodeId, subid);
+            var returnValue = (from nodemeter in nodeMeters
+                               join meter in _db.Meters on nodemeter.MeterId equals meter.ID
+                               join node in _db.Nodes on nodemeter.NodeId equals node.Id
+                               join parentNode in _db.Nodes on node.ParentId equals parentNode.Id
+                               join power in _db.MeterPowers on nodemeter.MeterPowerId equals power.Id
+                               join category in _db.MetersCategories on nodemeter.MeterCategoryId equals category.Id
+                               where parentNode.ParentId == 0
+                               select new { meter.ID, meter.SerialNumber, nodemeter.SubscriberId, node.Name, Power = power.Name, Category = category.Name, ParentNode = parentNode.Name }).ToList();
+            float pagecount = returnValue.Count;
             int count = (int)Math.Ceiling(pagecount / 10);
-            data = data.Skip(page * 10).TakeSafe(10).ToList();
-            var returnValue = from nodemeter in _db.NodeMeters
-                              join meter in _db.Meters on nodemeter.MeterId equals meter.ID
-                              join node in _db.Nodes on nodemeter.NodeId equals node.Id
-                              join parentNode in _db.Nodes on node.ParentId equals parentNode.Id
-                              join power in _db.MeterPowers on nodemeter.MeterPowerId equals power.Id
-                              join category in _db.MetersCategories on nodemeter.MeterCategoryId equals category.Id
-                              where parentNode.ParentId == 0
-                              select new { meter.ID, meter.SerialNumber, nodemeter.SubscriberId, node.Name, Power = power.Name, Category = category.Name, ParentNode = parentNode.Name };
+            returnValue = returnValue.Skip(page * 10).TakeSafe(10).ToList();
             responseMessage.Data = new { meter = returnValue, page = count };
             return Ok(responseMessage);
         }
---
+++ Resources/ExConverter.cs
@@ -0,0 +1,25 @@
+﻿using Aim.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aim.Core.Services.Resources
+{
+    public static class ExConverter
+    {
+        internal static List<NodeMeter> Filterize(List<NodeMeter> meters, int nodeId = 0, string subid = null)
+        {
+            List<NodeMeter> nodeMeters = meters;
+            if (nodeId > 0)
+            {
+                nodeMeters = nodeMeters.Where(a => a.NodeId == nodeId).ToList();
+            }
+            if (subid is object)
+            {
+                nodeMeters = nodeMeters.Where(a => a.SubscriberId.Contains(subid)).ToList();
+            }
+            return nodeMeters;
+        }
+    }
+}
